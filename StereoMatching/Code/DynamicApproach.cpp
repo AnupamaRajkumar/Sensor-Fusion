70368319dfaa7f8ac173955ad2ca23095b7522cf
Anupama <anupamar228@gmail.com>
2020-10-25 18:24:52 +0100

    Dynamic Programming tweaks

--- StereoMatching/Code/DynamicApproach.cpp
+++ StereoMatching/Code/DynamicApproach.cpp
@@ -11,7 +11,7 @@ Dynamic::Dynamic(cv::Mat& image1, cv::Mat& image2, int dmin, int window_size, do
 	this->dmin = dmin;
 	this->img1 = image1.clone();
 	this->img2 = image2.clone();
-	this->C = cv::Mat::zeros(image1.cols, image1.cols, image1.type());
+	this->C = cv::Mat::zeros(image1.cols, image1.cols, CV_32FC1);
 	this->M = cv::Mat::zeros(C.size(), CV_8UC1);
 }
 
@@ -19,7 +19,7 @@ Dynamic::Dynamic(cv::Mat& image1, cv::Mat& image2, int dmin, int window_size, do
 void Dynamic::DynamicApproachCalculation() {
 	cv::Mat naive_disparities = cv::Mat::zeros(this->img1.size(), CV_8UC1);
 	/*going through each horizontal scanline one by one*/
-//#pragma omp parallel for
+#pragma omp parallel for
 	for (int r = this->winSize / 2; r < this->img1.rows - this->winSize / 2; r++) {
 		std::vector<uchar> DSI = this->DisparitySpaceImage(r);
 		this->FindMinimumCostPath(DSI);
@@ -50,34 +50,38 @@ void Dynamic::FindMinimumCostPath(std::vector<uchar>& DSI) {
 
 	for (int r = 0; r < DSI.size(); r++) {
 		//right occlusion
-		C.at<uchar>(r, 0) = DSI[r];
-		M.at<uchar>(r, 0) = RightOcc;
+		C.at<float>(r, 0) = (r+1)*this->occlusion;
+		M.at<float>(r, 0) = LeftOcc;
 		//left occlusion
-		C.at<uchar>(0, r) = DSI[r];
-		M.at<uchar>(0, r) = LeftOcc;
+		C.at<float>(0, r) = (r+1)*this->occlusion;
+		M.at<uchar>(0, r) = RightOcc;
 	}
 
 
 	for (int r = 1; r < C.rows; r++) {
 		for (int c = 1; c < C.cols; c++) {
-			double min1, min2, min3, minVal;
+			//std::cout << r << "x" << c << std::endl;
+			int min1, min2, min3, minVal;
 			min1 = min2 = min3 = minVal = 0.;
-			min1 = C.at<uchar>(r - 1, c - 1) +  DSI[c];				//match
-			min2 = C.at<uchar>(r - 1, c) + this->occlusion;			//left occlusion
-			min3 = C.at<uchar>(r, c - 1) + this->occlusion;			//right occlusion
+			min1 = C.at<float>(r - 1, c - 1) +  DSI[c];				//match
+			min2 = C.at<float>(r - 1, c) + this->occlusion;			//left occlusion
+			min3 = C.at<float>(r, c - 1) + this->occlusion;			//right occlusion
 			/*determine the min of 3*/
 			minVal = min(min1, min(min2, min3));
 			/*Populate C and M matrices accordingly*/
-			C.at<uchar>(r, c) = minVal;
-			if (minVal == min1)
+			float epsilon = 0.0000001f;
+			C.at<float>(r, c) = minVal;
+			if (fabs(minVal - min1) < epsilon) {
 				M.at<uchar>(r, c) = Match;
-			if (minVal == min2)
+			}			
+			if (fabs(minVal - min2) < epsilon) {
 				M.at<uchar>(r, c) = LeftOcc;
-			if (minVal == min3)
+			}
+			if (fabs(minVal - min3) < epsilon) {
 				M.at<uchar>(r, c) = RightOcc;
+			}			
 		}
 	}
-	//cv::imwrite("CostMatrix.png", C);
 }
 
 /*http://www.epixea.com/research/multi-view-coding-thesisch3.html#ref-Bobick1999*/
@@ -89,12 +93,12 @@ void Dynamic::ReconstructOptimalPath(int r, cv::Mat& naive_disparities) {
 		//std::cout << int(M.at<uchar>(row, col)) << std::endl;
 		switch (int(M.at<uchar>(row, col))) {
 		case Match:
-			disparity = row;
+			disparity = abs(row - 1);
 			row--;
 			col--;
 			break;
 		case LeftOcc:
-			disparity = row - 1;
+			//disparity = row - 1;
 			row--;
 			break;
 		case RightOcc:
@@ -103,7 +107,7 @@ void Dynamic::ReconstructOptimalPath(int r, cv::Mat& naive_disparities) {
 			break;
 		}
 		//std::cout << "disparity:" << disparity << std::endl;
-		naive_disparities.at<uchar>(r - this->winSize / 2, col) = (disparity * 255) / dmin;				//;		
+		naive_disparities.at<uchar>(r - this->winSize / 2, col) = (disparity * 255) / this->dmin;
 		//std::cout << int(naive_disparities.at<uchar>(r - this->winSize / 2, col)) << std::endl;
 	}
 }
